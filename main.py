b8a6e04c80d155e53234ef6783bf9aa078318f26
George <gnavarrog1802@gmail.com>
2022-07-31 22:05:03 -0400

    Adding Enemies Inheritance from Sprite class

--- main.py
+++ main.py
@@ -104,7 +104,6 @@ class Player:
                     dy = tile[1].top - self.rect.bottom
                     self.vel_y = 0
 
-
         # update player coordinates
         self.rect.x += dx
         self.rect.y += dy
@@ -130,21 +129,25 @@ class World:
         row_count = 0
         for row in data:
             col_count = 0
-            for title in row:
-                if title == 1:
+            for tile in row:
+                if tile == 1:
                     img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
-                if title == 2:
+                if tile == 2:
                     img = pygame.transform.scale(grass_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                if tile == 3:
+                    blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+                    blob_group.add(blob)
+
                 col_count += 1
             row_count += 1
 
@@ -152,6 +155,25 @@ class World:
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
             pygame.draw.rect(screen, (255, 255, 255), tile[1], 2)
+
+
+class Enemy(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/blob.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_direction = 1
+        self.move_counter = 0
+
+    def update(self):
+        self.rect.x += self.move_direction
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
+            self.move_direction *= -1
+            self.move_counter *= -1
 
 
 # list for data
@@ -180,6 +202,9 @@ world_data = [
 
 
 player = Player(100, screen_height - 130)
+
+blob_group = pygame.sprite.Group()
+
 world = World(world_data)
 
 
@@ -193,6 +218,9 @@ while run:
     screen.blit(sun_img, (100, 100))
 
     world.draw()
+
+    blob_group.update()
+    blob_group.draw(screen)
 
     player.update()
 
